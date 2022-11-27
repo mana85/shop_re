650fb0820bfa5a8b3f5e58a4033605517e067c28
EC2 Default User <dd.flash85@gmail.com>
2022-11-27 05:52:51 +0000

    [edit]admin controller

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,18 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    if @order_detail.making_status == "production"
+      @order_detail.order.update(order_status: 2)
+    elsif OrderDetail.where(making_status: "completed", order_id: @order_detail.order_id).count == @order_detail.order.order_details.count
+      @order_detail.order.update(order_status: 3)
+    end
+    redirect_to request.referer
+  end
+
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
+
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,39 @@
 class Admin::ProductsController < ApplicationController
+
+  def index
+    @products = Product.page(params[:page])
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.update(product_params)
+    redirect_to admin_product_path(product)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:genre_id, :is_active, :name, :description, :price, :product_image)
+  end
 end
