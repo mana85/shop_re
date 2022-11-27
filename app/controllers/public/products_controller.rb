54dec2ce992eab70f8346d376b5f42754914ed6c
EC2 Default User <dd.flash85@gmail.com>
2022-11-27 08:56:44 +0000

    [edit]controller

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,25 @@
 class Public::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all.page(params[:page]).per(8)
+    @genres = Genre.all
+  end
+
+  def show
+    @products = Product.find(params[:id])
+    @cart_item = CartItem.new
+    @genres = Genre.all
+  end
+
+  def searches
+    @genres = Genre.all
+    @search_genre = params[:genre]
+    @products_all = Genre.search_for(@search_genre)
+    @products = Kaminari.paginate_array(Genre.search_for(@search_genre)).page(params[:page]).per(8)
+  end
+
+  def search_word
+    @genres = Genre.all
+  end
+
 end
--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,2 +1,34 @@
 class Public::ShippingAddressesController < ApplicationController
+
+  def index
+    @shipping_address = ShippingAddress.new
+    @shipping_addresses = current_customer.shipping_addresses.all
+  end
+
+  def create
+    @shipping_address = current_customer.shipping_addresses.new(shipping_address_params)
+    @shipping_address.save
+    redirect_to request.referer
+  end
+
+  def edit
+    @shipping_address = ShippingAddress.find(params[:id])
+  end
+
+  def update
+    @shipping_address = ShippingAddress.find(params[:id])
+    @shipping_address.update(shipping_address_params)
+    redirect_to shipping_addresses_path
+  end
+
+  def destroy
+    ShippingAddress.find(params[:id]).destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(:post_code,:address,:name,:phone_number)
+  end
 end
